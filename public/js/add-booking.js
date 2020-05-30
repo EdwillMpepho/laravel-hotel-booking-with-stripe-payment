bb058c553dbe2c65f76884f4b3939964adb34a2e
Mpepho Themba <mpephothemba@gmail.com>
2020-05-30 11:12:07 +0000

    Making final validation

--- public/js/add-booking.js
+++ public/js/add-booking.js
@@ -8,12 +8,28 @@ function getNrOfDays() {
     var checkOut = new Date(end);
     // put the returned number of days to the variable month days
     var monthDays = this.getDaysOfTheMonth(checkIn);
+    // checkIn months number
+    var checkInMonth = checkIn.getMonth();
+    // checkOut months number
+    var checkOutMonth = checkOut.getMonth();
+    // get  check In full year
+    var checkInYear = checkIn.getFullYear();
+    // get check Out full year
+    var checkOutYear = checkOut.getFullYear();
     // check if check in date is less than or to todays date
     if (today <= checkIn) {
         //check if check out date is less than check in date
         if (checkIn < checkOut) {
             if (checkIn.getDate() > checkOut.getDate()) {
-                days = checkIn.getDate() - (checkIn.getDate() - checkOut.getDate());
+               if (checkIn.getFullYear() <= (checkOut.getFullYear() + 1)) {
+                   if ((checkInMonth == checkOutMonth) || ( checkOutMonth == (checkInMonth + 1)) ) {
+                       days = checkIn.getDate() - (checkIn.getDate() - checkOut.getDate());
+                   } else {
+                       msg.innerHTML = 'Your booking days exceeds the limit of 32 days';
+                   }
+                } else{
+                    msg.innerHTML = 'Please rebook again an error occurred';
+                }
             } else if (checkOut.getDate() > checkIn.getDate()) {
                 days = checkOut.getDate() - checkIn.getDate();
             } else if (checkIn.getDate() === checkOut.getDate()) {
@@ -48,7 +64,8 @@ function clearMessage() {
     msg.innerHTML = "";
 }
 
-//get the days of the months
+//get the days of the months via checkIn
 function getDaysOfTheMonth(checkIn) {
     return new Date(checkIn.getFullYear(), checkIn.getMonth(), 0).getDate();
 }
+
